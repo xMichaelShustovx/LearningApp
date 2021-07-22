846c5cbf376d16f4500bce58c099e70786770cfb
Michael Shustov <shustovmm@gmail.com>
2021-07-22 20:07:22 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Michael Shustov on 22.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
