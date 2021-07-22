5d87d8df501b840d863b939544b78a7effa22e0c
Michael Shustov <shustovmm@gmail.com>
2021-07-22 22:47:34 +0300

    - finish homeview - create homeviewrow as an subview

--- LearningApp/Views/HomeView.swift
+++ LearningApp/Views/HomeView.swift
@@ -13,8 +13,33 @@ struct HomeView: View {
     
     var body: some View {
         
-        Text(String(model.modules[0].id))
-            .padding()
+        NavigationView {
+            
+            VStack(alignment: .leading) {
+                
+                Text("What's for today?")
+                    .padding(.leading, 20)
+                
+                ScrollView {
+                    
+                    LazyVStack {
+                        
+                        ForEach(model.modules) {module in
+                            
+                            VStack(spacing: 20) {
+                                // Learning card
+                                HomeViewRow(image: module.content.image, title: "Learn \(module.category)", description: module.content.description, count: "\(module.content.lessons.count) Lessons", time: module.content.time)
+                                
+                                // Test card
+                                HomeViewRow(image: module.test.image, title: "\(module.category) Test", description: module.test.description, count: "\(module.test.questions.count) Questions", time: module.test.time)
+                            }
+                        }
+                    }
+                    .padding()
+                }
+            }
+            .navigationTitle("Get Started")
+        }
     }
 }
 
